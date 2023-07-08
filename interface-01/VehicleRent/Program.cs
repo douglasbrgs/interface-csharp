e2999c778f063bafb40cbf5c427a597bf7ade4fd
Douglas Borges <douglasborges19@gmail.com>
2023-07-07 21:33:22 -0300

    finaliza solução sem interface

--- interface-01/VehicleRent/Program.cs
+++ interface-01/VehicleRent/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using VehicleRent.Entities;
+using VehicleRent.Services;
 
 namespace VehicleRent
 {
@@ -8,15 +9,28 @@ namespace VehicleRent
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Insira os dados do aluguel");
-            Console.Write("Modelo do carro: ");
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model: ");
             string model = Console.ReadLine();
-            Console.Write("Data Retirada (dd/MM/yyyy hh:mm): ");
+            Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
             DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
-            Console.Write("Data Devolução (dd/MM/yyyy hh:mm): ");
+            Console.Write("Return (dd/MM/yyyy hh:mm): ");
             DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
 
+            Console.Write("Enter price per hour: ");
+            double hourPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter price per day: ");
+            double dayPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
             CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            RentalService rentalService = new RentalService(hourPrice, dayPrice);
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine();
+            Console.WriteLine("INVOICE: ");
+            Console.WriteLine(carRental.Invoice);
         }
     }
 }
--- interface-01/VehicleRent/Services/RentalService.cs
+++ interface-01/VehicleRent/Services/RentalService.cs
@@ -1,4 +1,5 @@
-﻿using VehicleRent.Entities;
+﻿using System;
+using VehicleRent.Entities;
 
 namespace VehicleRent.Services
 {
@@ -7,15 +8,32 @@ namespace VehicleRent.Services
         public double PricePerHour { get; private set; }
         public double PricePerDay { get; private set; }
 
+        private BrazilTaxService _brazilTaxService = new BrazilTaxService();
+
         public RentalService(double pricePerHour, double pricePerDay)
         {
             PricePerHour = pricePerHour;
             PricePerDay = pricePerDay;
         }
 
-        public void ProcessInvoce(CarRental carRental)
+        public void ProcessInvoice(CarRental carRental)
         {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
 
+            double basicPayment = 0;
+
+            if (duration.TotalHours > 12)
+            {
+                basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+            }
+            else
+            {
+                basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours);
+            }
+
+            double tax = _brazilTaxService.Tax(basicPayment);
+
+            carRental.Invoice = new Invoice(basicPayment, tax);
         }
     }
 }
