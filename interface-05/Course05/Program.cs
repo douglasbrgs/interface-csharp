8dc8b5aadb4bfe21cd82b31e09abcd9b6240fe0c
Douglas Borges <douglasborges19@gmail.com>
2023-08-14 16:48:21 -0300

    Add Course05 - IComparable

---
+++ interface-05/Course05/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Course05.Entities;
+
+namespace Course05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"d:\temp\in.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+
+                    list.Sort();
+
+                    foreach (Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred: " + e.Message);
+            }
+        }
+    }
+}
